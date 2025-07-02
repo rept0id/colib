c559d7b6962e44cc8cb90e95d857c36f33cc8404
Rantouan Achmet <rad@simplecode.gr>
2025-07-02 16:08:43 +0300

    Create colib.h

---
+++ colib.h
@@ -0,0 +1,15 @@
+#ifndef COLIB_H
+#define COLIB_H
+
+#include <stdio.h>
+#include <omp.h>
+
+#define BEGIN do {
+#define END } while (0);
+
+#define COBEGIN _Pragma("omp parallel sections")
+#define COEND
+
+#define COSECTION _Pragma("omp section")
+
+#endif
